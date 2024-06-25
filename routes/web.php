7334c5f7778acd304d9e603b0fb9308d5c8b61eb
Jonyl Dave Villanueva <87189532+legendma13@users.noreply.github.com>
2024-06-25 22:37:01 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,6 @@ Route::group(['middleware' => ['auth', 'admin'], 'prefix' => 'admin', 'as' => 'a
     Route::resource('settings', SettingsController::class);
 
     Route::resource('attendance-records', AdminAttendanceRecordsController::class);
-    Route::resource('attendance-records', AttendanceRecordsController::class);
     // Route::get('records',[AdminAttendanceRecordsController::class, 'index'])->name('records');
     Route::get('records', [AdminAttendanceRecordsController::class, 'records'])->name('records');
     Route::get('merits-demerits/get', [AdminMeritanddemeritController::class, 'get']);
